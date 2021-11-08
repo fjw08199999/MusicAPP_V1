4bd0b656a19fc7a0b9a761ecd67f7bd7f2c51863
fred.fu <fjw08199999@gmail.com>
2021-11-08 10:53:39 +0800

    Initial Commit

---
+++ MusicAPP_V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAPP_V1
+//
+//  Created by fred fu on 2021/11/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
